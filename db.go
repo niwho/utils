081921d202dcfa06c20f9326fe8d1500549d5f09
wutao <wutao@imusics.com>
2018-07-03 22:33:25 +0900

    db config support timeout and maxopenconn

--- db.go
+++ db.go
@@ -23,14 +23,29 @@ func NewDBClient(name, server, user, passwd string) *DBClient {
 		User:     user,
 		Password: passwd,
 	}
-	if err := c.initdb(); err != nil {
+	if err := c.initdb(10, 250); err != nil {
 		fmt.Errorf("db init error=%v", err)
 	}
 	return c
 }
 
-func (db *DBClient) initdb() error {
+// timeout ms
+func NewDBClientV2(name, server, user, passwd string, maxConn int, timeout int) *DBClient {
+	c := &DBClient{
+		Name:     name,
+		Server:   server,
+		User:     user,
+		Password: passwd,
+	}
+	if err := c.initdb(maxConn, timeout); err != nil {
+		fmt.Errorf("db init error=%v", err)
+	}
+	return c
+}
+
+func (db *DBClient) initdb(maxConn, timeout int) error {
 	var err error
-	db.RealDb, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=100ms&readTimeout=50ms&writeTimeout=50ms", db.User, db.Password, db.Server, db.Name))
+	db.RealDb, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=100ms&readTimeout=%dms&writeTimeout=%dms", db.User, db.Password, db.Server, db.Name, timeout, timeout))
+	db.RealDb.DB().SetMaxOpenConns(maxConn)
 	return err
 }
