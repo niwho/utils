0c405ab094156597d32480e78e39599abf8911b1
wutao <wutao@imusics.com>
2018-08-07 12:01:19 +0900

    gzip add 2bytes

--- gzip.go
+++ gzip.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"compress/gzip"
 	"io"
+	"io/ioutil"
 )
 
 func Ungzip(r io.Reader) (io.ReadCloser, error) {
@@ -11,4 +12,13 @@ func Ungzip(r io.Reader) (io.ReadCloser, error) {
 		return nil, err
 	}
 	return rb, nil
+}
+
+func Ungzip2Bytes(r io.Reader) ([]byte, error) {
+	rb, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+	dat, err := ioutil.ReadAll(rb)
+	return dat, err
 }
