0700c8eab74a5c470ab50fa0b86ca1593d05e3c3
wutao <wutao@imusics.net>
2018-12-13 12:53:22 +0800

    log

--- redis.go
+++ redis.go
@@ -63,19 +63,20 @@ func (rc *RedisClient) Scan(pat string, offset int) (keys []string, iter int) {
 		conn.Close()
 	}()
 
-	if arr, err := redis.MultiBulk(conn.Do("SCAN", offset, "match", pat)); err == nil {
+	if arr, err := redis.MultiBulk(conn.Do("SCAN", offset, " match ", pat)); err == nil {
 		iter, _ = redis.Int(arr[0], nil)
 		keys, _ = redis.Strings(arr[1], nil)
 	}
-	logs.Log(logs.F{"iter": iter, "keys": keys}).Debug("Scan")
+	logs.Log(logs.F{"iter": iter, "keys": keys, "offset": offset}).Debug("Scan")
 	return
 
 }
 
 func (rc *RedisClient) GetAllKeys(pat string) (keys []string) {
 	iter := 0
+	var kks []string
 	for {
-		kks, iter := rc.Scan(pat, iter)
+		kks, iter = rc.Scan(pat, iter)
 		logs.Log(logs.F{"iter": iter, "kks": kks}).Debug("GetAllKeys")
 		keys = append(keys, kks...)
 		if iter == 0 {
