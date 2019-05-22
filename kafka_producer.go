002f5e3fd299b46385e555f93e9f760b0d3eb48c
wutao <wutao@rightpaddle.com>
2019-05-22 14:10:54 +0800

    with topic

--- kafka_producer.go
+++ kafka_producer.go
@@ -35,6 +35,13 @@ func SendMsgA(clientName string, key, data []byte) error {
 	return errors.New("not found")
 }
 
+func SendMsgAWithTopic(topic, clientName string, key, data []byte) error {
+	if cli, found := clients[clientName]; found {
+		return cli.AddMsgWithTopic(topic, key, data)
+	}
+	return errors.New("not found")
+}
+
 func SendLog(key, data []byte) error {
 	return kfkLogProducer.AddMsg(key, data)
 }
