ad04a2cd858a8b2e0ec673e57cf1e521c70bab37
wutao <wutao@gcu.edu.cn>
2021-10-03 01:59:06 +0800

    fix duplicated & add/modify id generator

--- id.go
+++ id.go
@@ -4,13 +4,32 @@ import (
 	"github.com/bwmarrin/snowflake"
 )
 
+var Default = &UIDFactory{}
+type UIDFactory struct {
+
+    IDFactory *snowflake.Node
+
+}
+
 var IDFactory *snowflake.Node
 
 func InitIDFactory(node int64) (err error) {
 	IDFactory, err = snowflake.NewNode(node)
+	Default = &UIDFactory {
+	    IDFactory: IDFactory
+	}
 	return
 }
 
+func NewIDFactory(node int64) (*snowflake.Node, error) {
+
+	factory, err = snowflake.NewNode(node)
+	return &UIDFactory {
+	    IDFactory: IDFactory
+	}, err
+}
+
+
 func GetNewID() int64 {
-	return int64(IDFactory.Generate())
+	return int64(Default.IDFactory.Generate())
 }
